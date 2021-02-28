c34a1ad6666908c80c9428b14057be93e0de2ec0
Shakeel Koper <shakeelkoper@gmail.com>
2021-02-28 20:25:27 +0530

    Banner UI WIP

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import bannerStyles from "./banner.module.css";
+import Hair from "../../images/hair.jpg"
+import Skin from "../../images/skin.jpg"
+import Weight from "../../images/weight.jpg"
+
+export default function Banner() {
+    return (
+        <div class={`d-flex align-items-center justify-content-center ${bannerStyles.bannerImage}`}>
+            <div class={`d-flex align-items-center flex-column text-center ${bannerStyles.bannerText}`}>
+                <h1>Women's Wellness Simplified</h1>
+                <p>We are the country’s first holistic wellness platform for men. We talk to men, listen to them and understand their needs</p>
+                <div className={bannerStyles.subTitle}>What's your concern?</div>
+                <ol className="productDisplay">
+                    <li>
+                        <img src={Hair} />
+                    </li>
+                    <li>
+                        <img src={Skin}/>
+                    </li>
+                    <li>
+                        <img src={Weight}/>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import Header from "../components/Header/header"
+import Banner from "../components/Banner/banner"
 import Container from "../components/container"
 
 export default function Home() {
   return (
     <Container>
       <Header/>
-      Hello world!
+      <Banner />
     </Container>
   )
 }
